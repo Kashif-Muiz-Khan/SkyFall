92740349db378ae828062e22a1648f4b4b7dbf97
Kashif-Muiz-Khan <khankashifmuiz@gmail.com>
2024-05-03 17:10:30 +0100

    Revert "Folder Renaming"
    This reverts commit 9783dfec82ac579ff4f48c00f60d396f6a57c8c3.

--- ALevelBlazorTemplate/Model/Quote.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SkyFall.Model
-{
-    public class Quote
-    {
-        public int QuoteID { get; set; }
-        public string QuoteText { get; set; }
-        public Author Author { get; set; }
-    }
-}
